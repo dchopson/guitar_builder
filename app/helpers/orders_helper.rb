1f9fb0b1b1d63723c82cc23a2d3a8d3676e624eb
Daniel Chopson <dchopson@gmail.com>
2015-03-22 14:30:20 -0400

    Working label/select helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -3,21 +3,15 @@ module OrdersHelper
     params[:action] == 'edit'
   end
 
-  # TODO this isn't working
-  # def guitar_select(form, label)
-  #   plural_label = label.to_s.pluralize
-  #   form.label label
-  #   form.select(label) do
-  #     Guitar.send(plural_label).each do |a|
-  #       content_tag(:option, a.first, value: a.first, data: {price: a.last})
-  #     end
-  #   end
-  # end
-
-  # <%= f.select(:city_id) do %>
-  #   <% [['Lisbon', 1], ['Madrid', 2]].each do |c| -%>
-  #     <%= content_tag(:option, c.first, value: c.last) %>
-  #   <% end %>
-  # <% end %>
-
+  def guitar_select(form, label)
+    tags = ''
+    Guitar.send(label.to_s.pluralize).each do |a|
+      tags += content_tag(:option, a.first, value: a.first, data: {price: a.last})
+    end
+    result = "#{form.label(label)}<br>"
+    result += form.select(label) do
+      tags.html_safe
+    end
+    result.html_safe
+  end
 end
