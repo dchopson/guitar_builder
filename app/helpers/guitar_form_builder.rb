8cf43aad055f7995bd45da9f16aac47f632f4d33
Daniel Chopson <dchopson@gmail.com>
2015-03-29 23:08:29 -0400

    Apply Bootstrap CSS, add more form helpers

--- app/helpers/guitar_form_builder.rb
+++ app/helpers/guitar_form_builder.rb
@@ -6,21 +6,33 @@ class GuitarFormBuilder < ActionView::Helpers::FormBuilder
     Guitar.send(label.to_s.pluralize).each do |a|
       tags += @template.content_tag(:option, a[:value], value: a[:value], data: a[:data])
     end
-    guitar_tag(label) do
-      @template.select(@object_name, label) { tags.html_safe }
+    tag_with_label(label) do
+      @template.select(@object_name, label, nil, {}, {class: 'form-control'}) { tags.html_safe }
     end
   end
 
   def guitar_checkbox(label)
-    guitar_tag(label) do
+    tag_with_label("#{label.to_s}?") do
       @template.check_box(@object_name, label)
+    end
+  end
+
+  def order_select(label)
+    tag_with_label(label) do
+      @template.select(@object_name, label, Order.send(label.to_s.pluralize), {}, {class: 'form-control'})
+    end
+  end
+
+  def order_text_field(label)
+    tag_with_label(label) do
+      @template.text_field(@object_name, label, class: 'form-control')
     end
   end
 
   private
 
-  def guitar_tag(label)
-    @template.content_tag(:div, class: 'field') do
+  def tag_with_label(label)
+    @template.content_tag(:div, class: 'form-group') do
       result = "#{@template.label(@object_name, label)}<br>"
       result += yield
       result.html_safe
