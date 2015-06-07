443a8d71b56cc8fab511a8faddae7d51b35ee240
Daniel Chopson <dchopson@gmail.com>
2015-06-07 12:50:45 -0400

    Add ActiveMerchant & PayPal Express gateway environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.after_initialize do
+    ActiveMerchant::Billing::Base.mode = :test
+    paypal_options = {
+      :login => ENV['PAYPAL_LOGIN'],
+      :password => ENV['PAYPAL_PASSWORD'],
+      :signature => ENV['PAYPAL_SIGNATURE']
+    }
+    ::EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(paypal_options)
+  end
 end
