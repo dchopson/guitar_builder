ce2851d7b04ef3d2abbf7d2f8aece75e0a1cacc0
Daniel Chopson <dchopson@gmail.com>
2016-11-15 16:58:31 -0500

    Temporarily remove domain restriction for demo purposes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -145,9 +145,9 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  # config.email_regexp = /\A[^@]+@[^@]+\z/
+  config.email_regexp = /\A[^@]+@[^@]+\z/
   # http://stackoverflow.com/a/4329965/1563183
-  config.email_regexp = /\A([\w\.%\+\-]+)@guitarbuilder\.com\z/i
+  # config.email_regexp = /\A([\w\.%\+\-]+)@guitarbuilder\.com\z/i
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
